eb186bf1faadb1b20fa907c7c82efc0097e1dfdb
maximys963 <maxim.p.jsdev@gmail.com>
2019-04-17 16:59:45 +0300

    Updates

---
+++ precache-manifest.0bd9a50a48968a8f760464e2208f950d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ca7d49699c23116d302b3f5cb6bc9e1",
+    "url": "/WL-Deploy/static/media/main.2ca7d496.jpg"
+  },
+  {
+    "revision": "79bd20bfcc792a936d13adfc6b0ba40c",
+    "url": "/WL-Deploy/static/media/delete.79bd20bf.jpg"
+  },
+  {
+    "revision": "441d8611106ab6473ed8c5487cda63ac",
+    "url": "/WL-Deploy/static/media/clear.441d8611.svg"
+  },
+  {
+    "revision": "209615a7b8188d7297fc",
+    "url": "/WL-Deploy/static/js/runtime~main.003666c2.js"
+  },
+  {
+    "revision": "285ab193394bba7422ca",
+    "url": "/WL-Deploy/static/js/main.8b64f998.chunk.js"
+  },
+  {
+    "revision": "8524b5f47969dbf808d9",
+    "url": "/WL-Deploy/static/js/2.9908a3a6.chunk.js"
+  },
+  {
+    "revision": "285ab193394bba7422ca",
+    "url": "/WL-Deploy/static/css/main.3ca74667.chunk.css"
+  },
+  {
+    "revision": "8524b5f47969dbf808d9",
+    "url": "/WL-Deploy/static/css/2.105c38fb.chunk.css"
+  },
+  {
+    "revision": "4b2a89eb5dd349db669121c546df5769",
+    "url": "/WL-Deploy/index.html"
+  }
+];
